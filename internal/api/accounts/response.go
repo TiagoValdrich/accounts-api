b385e006f61629024fcc557eb63f958a425e358b
Tiago Valdrich <tiago.valdrich@nubank.com.br>
2026-01-26 10:54:43 -0300

    fix: adjust response pattern for accounts endpoints

--- internal/api/accounts/response.go
+++ internal/api/accounts/response.go
@@ -7,15 +7,14 @@ import (
 )
 
 type AccountCreatedResponse struct {
-	ID        *uuid.UUID `json:"id"`
-	Document  string     `json:"document"`
+	ID        *uuid.UUID `json:"account_id"`
+	Document  string     `json:"document_number"`
 	CreatedAt time.Time  `json:"created_at"`
 }
 
 type SearchCustomerAccountByIDResponse struct {
-	ID        *uuid.UUID `json:"id"`
-	Document  string     `json:"document"`
-	CreatedAt time.Time  `json:"created_at"`
+	ID       *uuid.UUID `json:"account_id"`
+	Document string     `json:"document_number"`
 }
 
 func DomainToAccountCreatedResponse(customerAccountResult CustomerAccountResult) AccountCreatedResponse {
@@ -28,8 +27,7 @@ func DomainToAccountCreatedResponse(customerAccountResult CustomerAccountResult)
 
 func DomainToSearchAccountByIDResponse(searchCustomerAccountResult SearchCustomerAccountResult) SearchCustomerAccountByIDResponse {
 	return SearchCustomerAccountByIDResponse{
-		ID:        searchCustomerAccountResult.CustomerID,
-		Document:  searchCustomerAccountResult.Document,
-		CreatedAt: searchCustomerAccountResult.CreatedAt,
+		ID:       searchCustomerAccountResult.CustomerID,
+		Document: searchCustomerAccountResult.Document,
 	}
 }
